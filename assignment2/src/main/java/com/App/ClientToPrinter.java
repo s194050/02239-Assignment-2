3223c6fbb2d0dae863daf2eba8213f1f31b56cd8
William Mathiassen <s194050@dtu.dk>
2022-10-21 17:03:14 +0200

    Added functions from client to printer

--- assignment2/src/main/java/com/App/ClientToPrinter.java
+++ assignment2/src/main/java/com/App/ClientToPrinter.java
@@ -1,10 +1,18 @@
+package com.App;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ClientToPrinter extends Remote{ // Client to Printer interface
     public String echo(String input) throws RemoteException;
-
-    
+    public String print(String filename, String printer) throws RemoteException;
+    public String queue(String printer) throws RemoteException;
+    public String topQueue(String printer, int job) throws RemoteException;
+    public void Start() throws RemoteException;
+    public void Stop() throws RemoteException;
+    public void Restart() throws RemoteException;
+    public String status(String printer) throws RemoteException;
+    public String readConfig(String parameter) throws RemoteException;
+    public String setConfig(String parameter, String value) throws RemoteException;
     
 }
 
